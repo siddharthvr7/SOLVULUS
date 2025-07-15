f4210cae3ec8edad9b3eafa9b8d08c02deaa5848
siddharthvr7 <siddharthvr7@gmail.com>
2025-07-16 00:31:24 +0530

    Update solvulus.py

--- solvulus.py
+++ solvulus.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 import google.generativeai as genai
 
 load_dotenv()
-genai.configure(api_key=os.getenv("AIzaSyBJ5Fn-Bo-IEDTmubX-pRzz2HdOKEz6F_w"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
